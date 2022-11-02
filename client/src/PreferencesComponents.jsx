fab2081b4e529967a276f555f360a780391e68cc
larrrylee <102995712+larrrylee@users.noreply.github.com>
2022-11-01 21:35:17 -0700

    Added PreferencesComponents .jsx and .css stub

---
+++ client/src/PreferencesComponents.jsx
@@ -0,0 +1,20 @@
+import React from "react";
+import "./PreferencesComponents.css";
+
+const DietaryRestrictionsSearchBar = () => {
+    return (
+        <div className="dietary-restrictions">
+            // search bar, with boxes indicating selected restrictions (cf. https://dev.to/salehmubashar/search-bar-in-react-js-545l)
+            // option to click on selected restrictions to remove them
+            // need to implement the data structure for storing dietary restriction options
+        </div>
+    );
+}
+
+const MealSizeSelection = () => {
+    return (
+        <div className="meal-size-selection">
+            // select one among meal size options
+        </div>
+    );
+}
