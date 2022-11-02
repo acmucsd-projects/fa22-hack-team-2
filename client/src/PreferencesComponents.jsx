37bfbf775714871c1ac5671a3176ab420f06aa80
larrrylee <102995712+larrrylee@users.noreply.github.com>
2022-11-02 15:03:19 -0700

    Added PreferencesComponents handler stub

--- client/src/PreferencesComponents.jsx
+++ client/src/PreferencesComponents.jsx
@@ -1,16 +1,37 @@
 import React from "react";
 import "./PreferencesComponents.css";
 
-const DietaryRestrictionsSearchBar = () => {
+/* Search bar for Dietary Restrictions in Preferences. Automatic drop shows terms with closes correspondence. 
+ * Boxes under search bar indicate selected restrictions, and can be clicked to unselect restrictions. 
+ * MIRO: https://miro.com/app/board/uXjVPNa6HSs=/?moveToWidget=3458764537011712903&cot=14
+ */
+const DietaryRestrictionsSearchBar = () => {        // NOTE: Multiselect-react-dropdown is similar in function 
+                                                    // https://www.npmjs.com/package/multiselect-react-dropdown
+    // Hander for []
+    function handleChange(e) {
+        // TODO: implement handler
+    }
+    
     return (
         <div className="dietary-restrictions">
-            // search bar, with boxes indicating selected restrictions (cf. https://dev.to/salehmubashar/search-bar-in-react-js-545l)
-            // option to click on selected restrictions to remove them
-            // need to implement the data structure for storing dietary restriction options
+            <h1>Dietary Restrictions</h1>
+            <div className="search">
+                <TextField
+                    id="outlined-basic"
+                    onChange={handleChange}
+                    variant="outlined"
+                    fullWidth
+                    label="Dietary restrictions"
+                />
+            </div>
+            // TODO: need to implement the data structure for storing dietary restriction options
         </div>
     );
 }
 
+/* Single selection for Meal Size in Preferences. Options are {Meal, Snack, Drink}.
+ * MIRO: https://miro.com/app/board/uXjVPNa6HSs=/?moveToWidget=3458764537012001562&cot=14
+ */
 const MealSizeSelection = () => {
     return (
         <div className="meal-size-selection">
