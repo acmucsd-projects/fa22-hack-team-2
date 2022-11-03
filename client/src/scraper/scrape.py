f20502f282516856f8d8ff109ff66c5464d1c91a
jliu312 <85335934+jliu312@users.noreply.github.com>
2022-11-02 18:27:35 -0700

    Implemented Scraping Home Page (Formatting incorrect)

--- client/src/scraper/scrape.py
+++ client/src/scraper/scrape.py
@@ -1,4 +1,3 @@
-
 # This file currently is able to scrape the nutritional info of all
 # the food into into out.csv given a restauraunt link
 
@@ -9,14 +8,17 @@ from requests_html import HTML, HTMLSession
 import csv
 
 
+# to be prepended to each url (used across methods)
+urlBegin = 'https://hdh-web.ucsd.edu'
+
 # This method takes a URL of a food's nutritional 
 # page and formats it on the csv
 # @param url URL to be scraped
 
-def scrapeMacros(url):
+def scrapeMacros(url, csvName):
     print(url)
     # opens "out.txt" to be written on FOR DEBUGGING PURPOSES
-    f = open("out.txt", "a")
+    f = open(csvName, "a")
     source = requests.get(url).text
     soup = BeautifulSoup(source, 'lxml')
     # f.write(soup.prettify())
@@ -34,7 +36,7 @@ def scrapeMacros(url):
     textMacros = []
     textMacros.append(food.text)
     for i in range(len(macros)-2):
-        textMacros.append(macros[i].text)
+        textMacros.append(macros[i].text.replace("\n", ""))
 
     for macro in macros:
         f.write(macro.text + "\n")
@@ -45,6 +47,9 @@ def scrapeMacros(url):
     f.write(allergens + "\n")
     textMacros.append(allergens)
 
+    for macro in textMacros: 
+        print(macro)
+
     # append this row into out.csv
     with open('out.csv', 'a') as csv_file:
         writer = csv.writer(csv_file)
@@ -54,42 +59,62 @@ def scrapeMacros(url):
     f.close()
 
 
-# This block of code pulls the links of each food's nutritional facts
+# This method pulls the links of each food's nutritional facts
 # from the dining hall and calls the function scrapeMacros. 
+def scrapeRestauraunts(url): 
+    urls = []
+    source = requests.get('https://hdh-web.ucsd.edu/dining/apps/diningservices/Restaurants/MenuItem/64').text
+    # source = requests.get(url).text
+    soup = BeautifulSoup(source, 'lxml')
+    f = open("out.txt", "w")
+    # f.write(soup.prettify())
 
+    # find name of resturaunt
+    resturaunt = soup.find('h1').text
+    print(resturaunt)
 
-#
-urls = []
-source = requests.get('https://hdh-web.ucsd.edu/dining/apps/diningservices/Restaurants/MenuItem/64').text
+    # grab all the URLs with "nutritionfacts" in it
+    for links in soup.find_all('a', href=True):
+        direct = links.get("href")
+        if "nutritionfacts" in direct:
+            url = urlBegin + direct
+            urls.append(url)
+            f.write(url + "\n")
+
+    
+    # write the header for the csv
+    csvName = resturaunt + '.csv'
+    with open(csvName, 'w') as csv_file:
+        writer = csv.writer(csv_file)
+        writer.writerow(["Food", "Calories", "Calories from Fat",  
+                "Tot. Fat", "%DV", "Tot. Carb", "%DV", "Sat. Fat", 
+                "%DV","Dietary Fiber", "%DV", "Trans Fat", "%DV",
+                "Sugars", "%DV", "Cholesterol", "%DV", "Protein", 
+                "%DV", "Sodium", "%DV", "Allergens"])
+
+    # call scrapeMacros with each URL.
+    for url in urls:
+        scrapeMacros(url, csvName)
+        # f.write(url)
+
+    f.close()
+
+######
+source = requests.get('https://hdh-web.ucsd.edu/dining/apps/diningservices/').text
 soup = BeautifulSoup(source, 'lxml')
 f = open("out.txt", "w")
-# f.write(soup.prettify())
 
-# to be prepended to each url
-urlBegin = 'https://hdh-web.ucsd.edu'
+urls = set()
 
-# grab all the URLs with "nutritionfacts" in it
 for links in soup.find_all('a', href=True):
-    direct = links.get("href")
-    if "nutritionfacts" in direct:
-        url = urlBegin + direct
-        urls.append(url)
-        f.write(url + "\n")
+    directory = links.get("href")
+    f.write(directory + "\n")
+    if "Restaurants" in directory:
+        url = urlBegin + directory
+        urls.add(url)
 
- 
-# write the header for the csv
-with open('out.csv', 'w') as csv_file:
-    writer = csv.writer(csv_file)
-    writer.writerow(["Food", "Calories", "Calories from Fat",  
-            "Tot. Fat", "%DV", "Tot. Carb", "%DV", "Sat. Fat", 
-            "%DV","Dietary Fiber", "%DV", "Trans Fat", "%DV",
-            "Sugars", "%DV", "Cholesterol", "%DV", "Protein", 
-            "%DV", "Sodium", "%DV", "Allergens"])
+scrapeRestauraunts(url[0])
 
-# call scrapeMacros with each URL.
-for url in urls:
-    scrapeMacros(url)
-    # f.write(url)
-
-f.close()
-
+# for url in urls:
+#     print(url)
+#     scrapeRestauraunts(url)
