bb2a25398daeaeb301b49d6cad5c298c0af42e55
ZhuZiLiBryan <bryanzhuzili@gmail.com>
2022-11-27 11:08:50 -0800

    Parser file loads in Dining Items into database

--- client/src/convertDoc.py
+++ client/src/convertDoc.py
@@ -1,34 +1,34 @@
 import os
+import csv
 # Get the database using the method we defined in pymongo_test_insert file
 from pymongo_get_db import get_database
 
 # retrieve the database and collection
 dbname = get_database()
-collection_name = dbname["dining_items"]
-direc = "scraper/halls"
-
-# iterate over all CSV files in directory
-for file in os.listdir(direc):
-    print("bepis")
-
-
+collection_name = dbname["foods"]
+direc =  os.path.dirname(__file__) + "\scraper\halls"
 
 # parse the file and write its attributes to a document
 def parse_data(file):
     # get data field labels as keys
-    with open(file, "r") as f:
-        labels = f.readline().split(",")
+    with open(direc + "\\" + file, "r") as f:
+        reader = csv.DictReader(f, quoting=csv.QUOTE_ALL, skipinitialspace=True)
     
-    # parse each line and write it into a document for MongoDB
-    for line in f:
-        fields = line.split(",")
-        foodItem = {};
+        # parse each line and write it into a document for MongoDB
+        for foodItem in reader:
+            print(foodItem)
 
-        for idx, val in enumerate(fields):
-            foodItem[labels[idx].strip()] = val.strip()
+            # add food item into collection
+            collection_name.insert_one(foodItem)  
+            
 
-        # add food item into collection
-        collection_name.insert_one(foodItem)
+# iterate over all CSV files in directory
+for file in os.listdir(direc):
+    parse_data(file)
+
+
+
+
 
 
 
