80aacf3a2d0f8fe6d5e42be0a2f5b7b7cb876648
s3anAK <56090826+s3anAK@users.noreply.github.com>
2022-12-29 22:03:11 -0800

    initial edits

--- server/app.js
+++ server/app.js
@@ -2,23 +2,29 @@ const express = require('express');
 const logger = require('morgan');
 const mongoose = require('mongoose');
 const dotenv = require('dotenv');
-
 const usersRouter = require('./routes/users');
 
 const app = express();
+
+dotenv.config();
+
+app.listen(process.env.PORT, () => {
+  console.log('Server started on port ' + process.env.PORT);
+})
+app.use('/users', usersRouter);
+
+mongoose.connect(process.env.DB_URL, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true }).then(() => {
+console.log('Connected to MongoDB database');
+});
+
+/*
 
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
-app.use('/users', usersRouter);
-
-dotenv.config();
-
-mongoose.connect(process.env.DB_URL, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true }).then(() => {
-  console.log('Connected to MongoDB database');
-});
-
 module.exports = app;
+
+*/
