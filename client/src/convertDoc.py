10f767d2ec3c8c7ade80bf48872798a4c25cd059
ZhuZiLiBryan <bryanzhuzili@gmail.com>
2022-12-30 18:03:46 -0800

    Format web-parsed data into format of standard food schema

--- client/src/convertDoc.py
+++ client/src/convertDoc.py
@@ -1,29 +1,47 @@
 import os
 import csv
-# Get the database using the method we defined in pymongo_test_insert file
-from pymongo_get_db import get_database
+import json
 
 # retrieve the database and collection
-dbname = get_database()
-collection_name = dbname["foods"]
-direc =  os.path.dirname(__file__) + "\scraper\halls"
+csv_direc =  os.path.dirname(__file__) + "\scraper\halls"
+json_direc = os.path.dirname(__file__) + "\scraper\jsons"
+
+jsonArray = []
+
 
 # parse the file and write its attributes to a document
 def parse_data(file):
+    # dict to represent JSON of hall
+    hall = {"name":file, "food":[]}
+
     # get data field labels as keys
-    with open(direc + "\\" + file, "r") as f:
+    with open(csv_direc + "\\" + file, "r") as f:
         reader = csv.DictReader(f, quoting=csv.QUOTE_ALL, skipinitialspace=True)
     
-        # parse each line and write it into a document for MongoDB
+        # parse each line and write it into a JSON format
         for foodItem in reader:
-            print(foodItem)
+            # split allergens and restrictions into a list
+            allergensList = [allergen.strip() for allergen in foodItem["allergens"].split(",")]
+            restrictionsList = [restriction.strip() for restriction in foodItem["restrictions"].split(",")]
 
-            # add food item into collection
-            collection_name.insert_one(foodItem)  
+            foodItem["allergens"] = allergensList
+            foodItem["restrictions"] = restrictionsList
+
+            # add food to JSON array
+            jsonArray.append(foodItem)
+
+    # set food array to the key of food
+    hall["food"] = jsonArray
+
+    # write to json file
+    with open((json_direc + "\\" + file)[0:-4], "w", encoding = "utf-8") as f:
+        jsonStr = json.dumps(hall, indent = 4)
+        f.write(jsonStr)
+      
             
 
 # iterate over all CSV files in directory
-for file in os.listdir(direc):
+for file in os.listdir(csv_direc):
     parse_data(file)
 
 
