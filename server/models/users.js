7c7a6b744d439885502b4bd680bcc0a03dcf7d74
ZhuZiLiBryan <bryanzhuzili@gmail.com>
2022-12-30 20:48:49 -0800

    Update userschema

---
+++ server/models/users.js
@@ -0,0 +1,32 @@
+const mongoose = require('mongoose');
+
+const userSchema = new mongoose.Schema(
+    {
+        username: {
+            type: String,
+            required: true,
+        },
+
+        password: {
+            type: String,
+            required: true,
+        },
+
+        //Nuts, seafood, etc.
+        allergens: {
+            type: [String],
+            required: true,
+        },
+
+        //List of dietary restrictions (vegetarian, vegan, etc.)
+        restrictions: {
+            type: [String],
+            required: true,
+        }
+    }
+
+)
+
+const User = mongoose.model('user', userSchema);
+
+module.exports = User;
