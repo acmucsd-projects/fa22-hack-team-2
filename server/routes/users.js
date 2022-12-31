8831bcb3f131ae616f22bed2529c19135fd5a5e9
ZhuZiLiBryan <bryanzhuzili@gmail.com>
2022-12-30 20:38:43 -0800

    Update

--- server/routes/users.js
+++ server/routes/users.js
@@ -1,33 +1,44 @@
 const express = require('express');
+const { response } = require('../app');
 const router = express.Router();
 
-/* GET users listing. */
-/*
-router.get('/', function(req, res, next) {
-  const user = {
-    name: 'ACM Hack',
-    email: 'hack@acmucsd.org'
-  }
-  res.status(200).json({ user });
+// Get user model
+const User = require("../models/users");
+
+router.get("/register", async(req, res) => {
+    const users = await User.find().exec(); 
+    res.status(200).json({users});
 });
 
-module.exports = router;
-*/
 
-// Creating new user
-router.post("/users", (req, res) => {
-  const {newUser} = req.body;
-  const {/* have attributes here*/} = newUser;
-
-  if (/*failure conditions*/) {
+//Create new user when submitting create use request
+router.post("/register", async (req, res) => {
+  const newUsername = req.body.username;
+  const newPassword = req.body.password;
+  const confirmPassword = req.body.confirmPassword;
+  
+  //TODO: handle existing user credentials
+  if (!newUsername || !newPassword) {
     res.status(400);
     console.log("invalid user creation");
+  } else if (confirmPassword != newPassword) {
+    //TODO: handling if confirm password doesnt match 
+    console.log("passwords do not match");
   } else {
-    res.status(200).json({newUser})
+    const newUser = await User.create({
+      username:newUsername,
+      password:newPassword,
+    });
+    newUser.save()
+    .then(data => {
+      res.json(data);
+    })
+    .catch(error => {
+      res.json(error);
+    });
   }
-
-
 });
+
 
 
 module.exports = router;
