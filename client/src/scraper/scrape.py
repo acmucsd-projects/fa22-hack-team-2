ee7e5fc0181d15d74797be4b65d6e5dd841c572d
jliu312 <85335934+jliu312@users.noreply.github.com>
2023-01-04 18:49:04 -0800

    Updated scraper to match schema and include price

--- client/src/scraper/scrape.py
+++ client/src/scraper/scrape.py
@@ -20,7 +20,7 @@ data_folder = Path("scraper/scrapedDiningData/")
 # page and formats it on the csv
 # @param url URL to be scraped
 
-def scrapeMacros(url, csvName):
+def scrapeMacros(url, csvName, restrictions, price):
     print(url)
     # opens "out.txt" to be written on FOR DEBUGGING PURPOSES
     f = open("out.txt", "a")
@@ -35,6 +35,7 @@ def scrapeMacros(url, csvName):
     if(len(foodHTML) > 1):
         food = foodHTML[1]
         f.write(food.text + "\n")
+    else: 
         return
 
     # find the macros (all td tags)
@@ -47,19 +48,41 @@ def scrapeMacros(url, csvName):
     for i in range(len(macros)-2):
         textMacros.append(macros[i].text.replace("\n", ""))
 
-    for macro in macros:
-        f.write(macro.text + "\n")
+    # for macro in macros:
+    #     f.write(macro.text + "\n")
 
+    # strip everything away but the digits
+    for i in range(1, len(textMacros)):
+        num = ""
+        for c in textMacros[i]:
+            if c.isdigit() or c == '.':
+                num = num + c
+
+        # hard code away the extra decimal points
+        if(i == 5): 
+            num = num[2:]
+        elif(i == 7) :
+            num = num[1:]
+
+
+        textMacros[i] = num
+
+    # NOT USED ANYMORE, PASSED IN
     # find the allergens and append to the list
-    c = soup.find('div', class_='container-fluid max-width-1000')
-    allergensHTML = c.find_all('p')
-    if len(allergensHTML) > 4:
-        allergens = allergensHTML[4].text
-    f.write(allergens + "\n")
-    textMacros.append(allergens)
+    # c = soup.find('div', class_='container-fluid max-width-1000')
+    # allergensHTML = c.find_all('p')
+    # if len(allergensHTML) > 4:
+    #     allergens = allergensHTML[4].text
+    # f.write(allergens + "\n")
+    # textMacros.append(allergens)
+
+    textMacros.append(restrictions)
+    if "$" not in price:
+        price = ""
+    textMacros.append(price)
 
     # for macro in textMacros: 
-        # print(macro)
+    #     print(macro)
 
     # append this row into out.csv
     with open(csvName, 'a') as csv_file:
@@ -74,9 +97,11 @@ def scrapeMacros(url, csvName):
 # from the dining hall and calls the function scrapeMacros. 
 def scrapeRestauraunts(url): 
     urls = []
-    # source = requests.get('https://hdh-web.ucsd.edu/dining/apps/diningservices/Restaurants/MenuItem/64').text
     source = requests.get(url).text
     soup = BeautifulSoup(source, 'lxml')
+    # HTMLFile = open("web.html", "r")
+    # index = HTMLFile.read()
+    # soup = BeautifulSoup(index, 'lxml')
     f = open("out.txt", "w")
     # f.write(soup.prettify())
 
@@ -86,30 +111,47 @@ def scrapeRestauraunts(url):
     resturaunt = resturaunt.replace(' ', '')
     print(resturaunt)
 
-    # grab all the URLs with "nutritionfacts" in it
-    for links in soup.find_all('a', href=True):
-        direct = links.get("href")
-        if "nutritionfacts" in direct:
-            url = urlBegin + direct
-            urls.append(url)
-            f.write(url + "\n")
-
-    
     # write the header for the csv
     # csvName = data_folder / (resturaunt + '.csv')
     csvName = resturaunt + '.csv'
     with open(csvName, 'w') as csv_file:
         writer = csv.writer(csv_file)
-        writer.writerow(["Food", "Calories", "Calories from Fat",  
-                "Tot. Fat", "%DV", "Tot. Carb", "%DV", "Sat. Fat", 
-                "%DV","Dietary Fiber", "%DV", "Trans Fat", "%DV",
-                "Sugars", "%DV", "Cholesterol", "%DV", "Protein", 
-                "%DV", "Sodium", "%DV", "Allergens"])
+        writer.writerow(["food", "cal", "calFromFat",  
+                "totFat", "totFatDV", "totCarb", "totCarbDV", "satFat", 
+                "satFatDV","dietaryFiber", "dietaryFiberDV", "transFat", "transFatDV",
+                "sugars", "sugarsDV", "cholesterol", "cholesterolDV", "protein", 
+                "proteinDV", "sodium", "sodiumDV", "allergens", "price"])
 
+    # figure out each food's URL, Price, Allergens
+    for section in soup.find_all('li'):
+        for link in section.find_all('a', href=True):
+            f.write("OPAIWUJD:KLDSF    " + str(section) + "\n")
+            direct = link.get("href")
+            # find URL
+            price = ""
+            if "nutritionfacts" in direct:
+                url = urlBegin + direct
+                urls.append(url)
+
+                # find price
+                price = link.text.strip()
+                if price.find("$") > 1:
+                    price = price[price.find("$"):]
+            allergens = ""
+            for image in section.find_all('img', alt=True):
+                txt = image.get("alt")
+                f.write(txt + "\n")
+                if "Legend:" in txt:
+                    allergens = allergens + txt[8:-5] +", "
+            # print(allergens)
+            scrapeMacros(url, csvName, allergens, price)
+
+
+        
     # call scrapeMacros with each URL.
-    for url in urls:
-        scrapeMacros(url, csvName)
-        # f.write(url)
+    # for url in urls:
+    #     scrapeMacros(url, csvName, 0, 0)
+    #     f.write(url)
 
     f.close()
 
@@ -119,6 +161,7 @@ def scrapeRestauraunts(url):
 # runs scrapeResturaunts with all the URLs of the resturaunts.
 source = requests.get('https://hdh-web.ucsd.edu/dining/apps/diningservices/').text
 soup = BeautifulSoup(source, 'lxml')
+
 f = open("out.txt", "w")
 
 # URLs has to be a set here, since there are multiple instances of each URL.
@@ -126,7 +169,7 @@ urls = set()
 
 for links in soup.find_all('a', href=True):
     directory = links.get("href")
-    f.write(directory + "\n")
+    # f.write(directory + "\n")
     if "Restaurants" in directory:
         url = urlBegin + directory
         urls.add(url)
@@ -134,7 +177,6 @@ for links in soup.find_all('a', href=True):
 # scrapeRestauraunts(url[0])
 
 for url in urls:
-    print(url)
     scrapeRestauraunts(url)
 
 
